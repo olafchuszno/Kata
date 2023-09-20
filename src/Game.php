c79524e5bc7519093840b09951cc9438382a8700
Olaf <olaf.chuszno@gmail.com>
2023-09-20 18:24:25 +0200

    Implement Game to test until the 9th round

--- src/Game.php
+++ src/Game.php
@@ -8,16 +8,105 @@ class Game
 
     protected array $rolls = [];
 
+    // pins variable holds the count of actual pins knocked
+    protected array $pins = [];
+
+    protected $bonus = 0;
+
+    protected $addToBonus = 0;
+
+
+
+
 
     public function roll(int $pins)
     {
+        // update the 'pins' array about the actual number of pins knocked
+        $this->pins[] = $pins;
+
+
+        // Check for STRIKE or SPARE
+        if ($this->strike($pins)) {
+
+            // It's' a STRIKE! Increment the addToBonus variable by 2
+            $this->addToBonus += 2;
+
+        } else if ($this->spare($pins)) {
+
+            // It's a SPARE! Increment the addToBonus variable by 2
+            $this->addToBonus += 1;
+        }
+
+
+        // Check whether bonus applies to this round
+        if ($this->bonus > 0) {
+            // Apply bonus to this roll's score
+            $pins *= 2;
+
+            // Decrement the bonus
+            $this->bonus -= 1;
+        }
+
+        // Increment the bonus variable for next round/s
+        $this->bonus += $this->addToBonus;
+        
+        // Reset addToBonus variable
+        $this->addToBonus = 0;
+
+        // Return the score for this roll
         return $this->rolls[] = $pins;
     }
+
+
+
+
 
 
     public function score()
     {
         return array_sum($this->rolls);
+    }
+
+
+
+
+
+
+    // STRIKE
+    protected function strike(int $pins)
+    {   
+        // If it is the first out of the 2 throws
+        if (count($this->rolls) == 0 || count($this->rolls) % 2 == 0) {
+
+            // If all pins were knocked down at once
+            if ($pins == 10) {
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+
+
+    // SPARE
+    protected function spare(int $pins)
+    {
+        // If it is the second out of the 2 throws
+        if (count($this->rolls) != 0 && count($this->rolls) % 2 != 0) {
+
+            // If all pins from the last round (2 throws)
+            if (end($this->pins) + $pins == 10) {
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -32,11 +32,29 @@ class GameTest extends TestCase
     {
         $game = new Game();
 
-        foreach(range(1, 20) as $roll) {
+        foreach (range(1, 20) as $roll) {
             $game->roll(0);
         }
 
         $this->assertSame(0, $game->score());
+
+    }
+
+    /** @test */
+    public function it_awards_a_one_roll_bonus_for_every_spare()
+    {
+        $game = new Game();
+
+        $game->roll(5);
+        $game->roll(5);
+
+        $game->roll(8);
+
+        // foreach (range(1, 17) as $roll) {
+        //     $game->roll(0);
+        // }
+
+        $this->assertEquals(26, $game->score());
 
     }
 
